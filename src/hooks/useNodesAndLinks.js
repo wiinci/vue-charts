8815b7edea31e135c1d800bba83bc9006daa58da
Mardav Wala <tru.vinci@gmail.com>
2022-04-27 19:22:03 -0400

    [patch] Refactor using proxyRefs
    Signed-off-by: Mardav Wala <tru.vinci@gmail.com>

--- src/hooks/useNodesAndLinks.js
+++ src/hooks/useNodesAndLinks.js
@@ -4,36 +4,47 @@
  * @returns {object} the sankey nodes and links
  */
 import { sankey, sankeyJustify, sankeyLeft } from 'd3-sankey';
-import { computed } from 'vue';
+import { computed, proxyRefs } from 'vue';
 
 const useNodesAndLinks = props => {
+  const {
+    data,
+    height,
+    marginBottom,
+    marginLeft,
+    marginRight,
+    marginTop,
+    nodeAlign,
+    nodeId,
+    nodePadding,
+    nodeWidth,
+    sort,
+    width,
+  } = proxyRefs(props);
+
   const nodeById = new Map();
 
   const align = computed(() =>
-    props.nodeAlign === 'justify' ? sankeyJustify : sankeyLeft
+    nodeAlign === 'justify' ? sankeyJustify : sankeyLeft
   );
 
-  const chartHeight = computed(
-    () => props.height - props.marginTop - props.marginBottom
-  );
+  const chartHeight = computed(() => height - marginTop - marginBottom);
 
-  const chartWidth = computed(
-    () => props.width - props.marginLeft - props.marginRight
-  );
+  const chartWidth = computed(() => width - marginLeft - marginRight);
 
-  const offsetStart = computed(() => props.marginLeft + props.marginRight);
+  const offsetStart = computed(() => marginLeft + marginRight);
 
-  const offsetEnd = computed(() => props.marginTop + props.marginBottom);
+  const offsetEnd = computed(() => marginTop + marginBottom);
 
-  const sort = computed(() => (props.sort ? null : undefined));
+  const sorted = computed(() => (sort ? null : undefined));
 
   const fn = computed(() =>
     sankey()
       .nodeAlign(align.value)
-      .nodeId(d => d[props.nodeId])
-      .nodePadding(props.nodePadding)
-      .nodeSort(sort.value)
-      .nodeWidth(props.nodeWidth)
+      .nodeId(d => d[nodeId])
+      .nodePadding(nodePadding)
+      .nodeSort(sorted.value)
+      .nodeWidth(nodeWidth)
       .extent([
         [offsetStart.value, offsetEnd.value],
         [chartWidth.value, chartHeight.value],
@@ -41,27 +52,27 @@ const useNodesAndLinks = props => {
   );
 
   // Sankey requires a value (or weight) and since we're showing a lineage, we need to set the value to 1
-  if (!props.data[0].value) {
-    props.data.map(d => (d.value = 1));
+  if (!data[0].value) {
+    data.map(d => (d.value = 1));
   }
 
-  for (const link of props.data) {
+  for (const link of data) {
     if (!nodeById.has(link.source)) {
       nodeById.set(link.source, {
-        [props.nodeId]: link.source,
+        [nodeId]: link.source,
       });
     }
 
     if (!nodeById.has(link.target)) {
       nodeById.set(link.target, {
-        [props.nodeId]: link.target,
+        [nodeId]: link.target,
       });
     }
   }
 
   const sankeyData = {
     nodes: Array.from(nodeById.values()),
-    links: props.data,
+    links: data,
   };
 
   const { nodes, links } = fn.value({
