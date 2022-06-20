eaf758bf4e89527129dc7073e5fc411884841194
Mardav Wala <tru.vinci@gmail.com>
2022-06-19 20:14:33 -0400

    config: Enable reactivity transform

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
-import vue from "@vitejs/plugin-vue";
-import { fileURLToPath, URL } from "url";
-import { defineConfig } from "vite";
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'url';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      reactivityTransform: true,
+    }),
+  ],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 });
