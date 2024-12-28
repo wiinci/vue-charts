a2494d70d6f1d01783efd55bfed3e8706377a17b
Mardav Wala <tru.vinci@gmail.com>
2024-12-28 18:01:06 -0500

    style: Format code for consistency and readability

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,50 @@
 <script setup>
-import Sankey from '@/components/Sankey/Sankey.vue'
-import LineChart from '@/components/LineChart/LineChart.vue'
-import {ref} from 'vue'
+	import Sankey from '@/components/Sankey/Sankey.vue'
+	import LineChart from '@/components/LineChart/LineChart.vue'
+	import { ref } from 'vue'
 
-const nodeAlign = ref('left')
-const nodeId = ref('id')
-const nodePadding = ref(1e9)
-const nodeWidth = ref(1e-9)
-const sort = ref(false)
+	const nodeAlign = ref('left')
+	const nodeId = ref('id')
+	const nodePadding = ref(1e9)
+	const nodeWidth = ref(1e-9)
+	const sort = ref(false)
 </script>
 
 <script>
-const sankeyData = (await import('@/data/edges2.json')).default
-const lineData = (await import('@/data/aapl.csv?raw')).default
+	const sankeyData = (await import('@/data/edges2.json')).default
+	const lineData = (await import('@/data/aapl.csv?raw')).default
 
-export default {
-	setup() {
-		return {data}
-	},
-}
+	export default {
+		setup() {
+			return { data }
+		},
+	}
 </script>
 
 <template>
-	<!-- <Sankey
+	<Sankey
 		:data="sankeyData"
 		:node-align="nodeAlign"
 		:node-id="nodeId"
 		:node-padding="nodePadding"
 		:node-width="nodeWidth"
 		:sort="sort"
-	/> -->
+	/>
 	<LineChart :data="lineData" />
 </template>
 
 <style>
-:root {
-	--font-family-system: BlinkMacSystemFont, -apple-system, 'Segoe UI', Roboto,
-		Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
-	--font-family-monospace: 'SF Mono', 'Roboto Mono', Menlo, monospace;
-}
-path {
-	mix-blend-mode: multiply;
-}
-#app {
-	font-family: var(--font-family-system);
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-}
+	:root {
+		--font-family-system: BlinkMacSystemFont, -apple-system, 'Segoe UI', Roboto,
+			Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
+		--font-family-monospace: 'SF Mono', 'Roboto Mono', Menlo, monospace;
+	}
+	path {
+		mix-blend-mode: multiply;
+	}
+	#app {
+		font-family: var(--font-family-system);
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+	}
 </style>
